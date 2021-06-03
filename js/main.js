12c044f7efd9f6008c400c7801e55ca05bda1990
= <liliasalas@lilias-imac.myfiosgateway.com>
2021-06-02 22:10:52 -0400

    buttons work

---
+++ js/main.js
@@ -0,0 +1,40 @@
+// ==============DOM Elements ======================
+
+const plusButton = document.getElementById('plus');
+console.log(plusButton);
+
+const minusButton = document.getElementById('minus');
+console.log(minusButton);
+
+const input = document.getElementById('input');
+console.log(input);
+
+const h2 = document.getElementById('result');
+console.log(h2);
+
+
+
+// ==================Functions ======================
+
+let result = 0;
+
+const add = () => {
+ const selectingInput = parseInt(document.getElementById('input').value);
+ result += selectingInput;
+ h2.innerHTML = result;
+}
+
+
+const subtract = () => {
+ const selectingInput = parseInt(document.getElementById('input').value);
+ result -= selectingInput;
+ h2.innerHTML = result;
+}
+
+
+
+
+// ==============Event Listeners =================
+
+plusButton.addEventListener('click', add);
+minusButton.addEventListener('click', subtract);
